a8f2a0635a396ab1f179818a4e33b7585d655dc9
pol <polfalguera@DESKTOP-4PLUJVT.localdomain>
2022-11-06 13:19:30 +0100

    Implementació CtrlDomini

--- src/ControladorDomini.java
+++ src/ControladorDomini.java
@@ -1,12 +1,51 @@
 package src;
 
+import java.util.*;
 
 public class ControladorDomini {
 
     private ConjuntDocuments cjtDocuments;
 
-    
-    public int crearDocument(String titol, String autor) { return cjtDocuments.crearDocument(titol,autor); }
+    public void queryCrearDocument(String titol, String autor, String status) {
+        cjtDocuments.crearDocument(titol,autor,status);
+
+        if (!status.equals(" ")) System.out.println(status);
+    }
+
+    public void queryEliminarDocument(String titol, String autor, String status) {
+        cjtDocuments.eliminarDocument(titol,autor,status);
+
+        if (!status.equals(" ")) System.out.println(status);
+    }
+
+    public List<String> queryLlistarTitolsAutor(String autor, String status) {
+        List<String> llistat = cjtDocuments.llistarTitolsAutor(autor,status);
+
+        if (!status.equals(" ")) System.out.println(status);
+
+        return llistat;
+    }
+
+    public List<String> queryLlistarAutorsPrefix(String prefix, String status) {
+        List<String> llistat = cjtDocuments.llistarAutorsPrefix(prefix,status);
+
+        if (!status.equals(" ")) System.out.println(status);
+
+        return llistat;
+    }
+
+    public String queryGetContingutDocument(String titol, String autor, String status) {
+        int idDoc = cjtDocuments.indexContingutDocument(autor,titol,status);
+
+        if (idDoc == -1) {
+            System.out.println(status);
+            return " ";
+        }
+
+        String contingut = " "; //CtrlContingut.getContingut(idDoc,status);
+
+        return contingut;
+    }
 
 }
 
