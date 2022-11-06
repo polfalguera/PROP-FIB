11f5596d10407067a296dbd7a24f24bba589a922
Alex Ares Marin <alexares@MacBook-Pro-de-Alex.local>
2022-11-06 12:15:58 +0100

    readFromText

--- src/tfIdf.java
+++ src/tfIdf.java
@@ -1,4 +1,7 @@
 package src;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class tfIdf {
@@ -33,17 +36,17 @@ public class tfIdf {
         }
     }
 
-    private static double tf(String paraula, String[] contingut) {
+    private static double tf(String paraula, List<String> contingut) {
         double result = 0;
-        for (int i = 0; i < contingut.length; ++i) {
-            if (contingut[i].equalsIgnoreCase(paraula)) ++result;
+        for (int i = 0; i < contingut.size(); ++i) {
+            if (contingut.get(i).equalsIgnoreCase(paraula)) ++result;
         }
-        return result / contingut.length;
+        return result / contingut.size();
     }
 
-    private static double idf(String paraula, List<String[]> documents) {
+    private static double idf(String paraula, List<List<String>> documents) {
         double n = 0;
-        for (String[] doc : documents) {
+        for (List<String> doc : documents) {
             for (String word : doc) {
                 if (paraula.equalsIgnoreCase(word)) {
                     ++n;
@@ -53,7 +56,7 @@ public class tfIdf {
         return Math.log(documents.size() / n);
     }
 
-    public static int[] termsTfIdf(String[] paraules, List<String[]> documents, int k) {
+    public static int[] termsTfIdf(String[] paraules, List<List<String>> documents, int k) {
         if (k > documents.size()) k = documents.size();
         double[] tfidf = new double[documents.size()];
         for (int i = 0; i < tfidf.length; ++i) tfidf[i] = 0;
@@ -65,6 +68,36 @@ public class tfIdf {
             }
         }
         return IndexValuePair.krellevants(tfidf, k);
+    }
+
+    public static List<String> llegirFromText(String pathFile) throws IOException {
+        String line;
+        FileReader file = new FileReader(pathFile);
+        BufferedReader br = new BufferedReader(file);
+        List<String> text = new ArrayList<String>();
+        //Gets each line till end of file is reached
+        while((line = br.readLine()) != null) {
+            //Splits each line into words
+            String words[] = line.split((" |,|\\.|!|¡|\\?|¿"));
+            for (int i = 0; i < words.length; ++i)
+                if (words[i] != "") text.add(words[i]);
+        }
+        return text;
+    }
+
+    public static List<String> llegirFromString(String contingut) {
+        List<String> text = new ArrayList<String>();
+        String words[] = contingut.split((" |,|\\.|!|¡|\\?|¿"));
+        for (int i = 0; i < words.length; ++i)
+            if (words[i] != "") text.add(words[i]);
+        return text;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<String> text = llegirFromText("/Users/alexares/Desktop/subgrup-prop11.3/src/data.txt");
+        System.out.println(text);
+        text = llegirFromString("hola que tal, espero que separi be ¡jaja!");
+        System.out.println(text);
     }
 }
 
