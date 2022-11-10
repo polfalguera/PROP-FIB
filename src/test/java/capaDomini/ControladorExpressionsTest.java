952ada64ddbf2fc57082e9190117ced65663559a
haonan.jin1 <haonan.jin1@estudiantat.upc.edu>
2022-11-10 23:15:40 +0100

    Test

---
+++ src/test/java/capaDomini/ControladorExpressionsTest.java
@@ -0,0 +1,25 @@
+package capaDomini;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControladorExpressionsTest {
+
+    @Test
+    void anadir_expressio() {
+
+    }
+
+    @Test
+    void deleteExpressio() {
+    }
+
+    @Test
+    void setExpressio() {
+    }
+
+    @Test
+    void consultaExpressioBooleana() {
+    }
+}
