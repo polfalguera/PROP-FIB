d30c5370b83d75fe0556b13c86d76d20623c2cd5
pol <polfalguera@DESKTOP-4PLUJVT.localdomain>
2022-11-28 20:15:54 +0100

    Experimentant amb Swing

--- FONTS/Presentacio/ControladorPresentacio.java
+++ FONTS/Presentacio/ControladorPresentacio.java
@@ -4,27 +4,22 @@ import FONTS.Domini.ControladorDomini;
 
 public class ControladorPresentacio {
 
-    private ControladorDomini ctrlDomain;
+    private ControladorDomini CtrlDomini;
 
     private final MainView vistaPrincipal;
 
     public ControladorPresentacio() throws Exception {
-        this.ctrlDomain = new ControladorDomini();
-        this.vistaPrincipal = new MainView(this);
+        this.CtrlDomini = new ControladorDomini();
+        this.vistaPrincipal = new MainView("Document Manager");
     }
 
     public static void main (String[] args) {
-        javax.swing.SwingUtilities.invokeLater (
-                () -> {
-                    ControladorPresentacio ctrlPresentation = null;
-                    try {
-                        ctrlPresentation = new ControladorPresentacio();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    assert ctrlPresentation != null;
-                    //ctrlPresentation.openMainView();
-                });
+        try {
+            ControladorPresentacio CtrlPresentacio = new ControladorPresentacio();
+            CtrlPresentacio.vistaPrincipal.setVisible(true);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
--- FONTS/Presentacio/MainView.java
+++ FONTS/Presentacio/MainView.java
@@ -1,121 +1,104 @@
 package FONTS.Presentacio;
 
+import org.w3c.dom.ls.LSOutput;
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainView extends JFrame {
+    private JPanel mainPanel;
+    private JMenuBar MenuBar;
+    private JMenu File;
+    private JMenu Export;
+    private JMenu Edit;
+    private JMenu Help;
+    private JMenuItem Open;
+    private JMenuItem NewDoc;
+    private JMenuItem Save;
+    private JMenuItem SaveAs;
+    private JMenuItem txt;
+    private JMenuItem xml;
+    private JMenuItem jamp;
+    private JMenuItem Copy;
+    private JMenuItem Paste;
+    private JMenuItem Cut;
 
-    private JMenu file, edit,help,Export;
-    private JMenuItem cut,copy,paste,selectAll, New,Open,Save,Save_as,txt,xml,jamp;
-    private JMenuBar mb = new JMenuBar();
-    private final ControladorPresentacio iCtrlPresentation;
-    private JPanel panel1;
-    private JButton button1;
-    private JTextArea ta;
+    private JTextArea Content;
 
-    /**
-     * Inicialitza els components de la Vista Principal i assigna els listeners corresponents.
-     */
-    private void initializeComponents() {
+
+    public void initializeMenuBar() {
+        this.MenuBar = new JMenuBar();
+
+        //Initializing items
+        this.Open = new JMenuItem("Open");
+        this.NewDoc = new JMenuItem("New document");
+        this.Save = new JMenuItem("Save file");
+        this.SaveAs = new JMenuItem("Save file as...");
+        this.txt = new JMenuItem(".txt");
+        this.xml = new JMenuItem(".xml");
+        this.jamp = new JMenuItem(".jamp");
+        this.Copy = new JMenuItem("Copy");
+        this.Paste = new JMenuItem("Paste");
+        this.Cut = new JMenuItem("Cut");
+
+        //Initializing menus
+        this.File = new JMenu("File");
+        this.Export = new JMenu("Export");
+        this.Edit = new JMenu("Edit");
+        this.Help = new JMenu("Help");
+
+        //Adding items
+        this.File.add(Open);
+        this.File.add(NewDoc);
+        this.File.add(Save);
+        this.File.add(SaveAs);
+        this.Export.add(txt);
+        this.Export.add(xml);
+        this.Export.add(jamp);
+        this.Edit.add(Copy);
+        this.Edit.add(Paste);
+        this.Edit.add(Cut);
+
+        //Adding menus
+        this.MenuBar.add(File);
+        this.File.add(Export);
+        this.MenuBar.add(Edit);
+        this.MenuBar.add(Help);
+
+        this.setJMenuBar(MenuBar);
+    }
+
+    public void initializeListeners() {
+
+        Copy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               Content.copy();
+            }
+        });
+
+        Paste.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Content.paste();
+            }
+        });
+
+    }
+
+    public MainView(String title) {
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+
+        this.setSize(750,750);
         initializeMenuBar();
-        assign_listenerComponents();
-        panel1.setVisible(true);
-        button1.setVisible(true);
+        Content = new JTextArea();
+        this.add(Content);
+        initializeListeners();
     }
-
-    /**
-     * Inicialitza els botons del menú de la Vista Principal.
-     */
-    private void initializeMenuBar() {
-
-        JFrame f= new JFrame("La nostra Aplicacio");
-
-
-        ta=new JTextArea();
-        ta.setBounds(5,5,360,320);
-        f.add(ta);
-
-        file =new JMenu("File");
-        edit = new JMenu("Edit");
-        help = new JMenu("Help");
-        Export = new JMenu("Export");
-
-        cut =new JMenuItem("cut");
-        paste =new JMenuItem("paste");
-        selectAll =new JMenuItem("selectAll");
-        New = new JMenuItem("New");
-        Open = new JMenuItem("Open");
-        Save = new JMenuItem("Save");
-        Save_as = new JMenuItem("Save as");
-        txt = new JMenuItem("txt");
-        xml = new JMenuItem("uml");
-        jamp = new JMenuItem("jamp");
-
-        Export.add(txt);Export.add(xml); Export.add(jamp);
-        file.add(New);file.add(Open);file.add(Save);file.add(Save_as);file.add(Export);
-        edit.add(cut); edit.add(paste); edit.add(selectAll);
-        mb.add(file);
-        mb.add(edit);
-        mb.add(help);
-        f.setJMenuBar(mb);
-        f.setSize(400,400);
-        f.setLayout(null);
-        f.setVisible(true);
-    }
-
-    /**
-     * Constructora de la Vista Principal.
-     *
-     * @param pCtrlPresentation controlador presentació.
-     */
-    public MainView(ControladorPresentacio pCtrlPresentation) {
-        iCtrlPresentation = pCtrlPresentation;
-        initializeComponents();
-    }
-    public void actionPerformed (ActionEvent e) throws Exception {
-        Object source = e.getSource();
-
-        if(e.getSource()==cut)
-            ta.cut();
-        if(e.getSource()==paste)
-            ta.paste();
-        if(e.getSource()==copy)
-            ta.copy();
-        if(e.getSource()==selectAll)
-            ta.selectAll();
-    }
-
-    private void assign_listenerComponents() {
-
-        New.addActionListener(e -> {
-            try {
-                actionPerformed(e);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-
-        paste.addActionListener(e -> {
-            try {
-                actionPerformed(e);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-        cut.addActionListener(e -> {
-            try {
-                actionPerformed(e);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-        selectAll.addActionListener(e -> {
-            try {
-                actionPerformed(e);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-    }
-
 }
